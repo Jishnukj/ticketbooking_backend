69c132be6a2cdb0d3daa4e6c0d6cae1039410ebd
Jishnukj <jishnukjayan97@gmail.com>
2021-02-22 18:07:06 +0530

    venue repo

---
+++ Ticket_Booking/DataService/Repo/IVenueRepo.cs
@@ -0,0 +1,14 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Repo
+{
+    public interface IVenueRepo
+    {
+        bool addVenue(Venue venue);
+        Venue getVenuebyId(int id);
+        List<Venue> getAllVenues();
+    }
+}
---
+++ Ticket_Booking/DataService/Repo/VenueRepo.cs
@@ -0,0 +1,34 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataService.Repo
+{
+    public class VenueRepo:IVenueRepo
+    {
+        private readonly ApplicationdbContext _dbContext;
+        public VenueRepo(ApplicationdbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Venue getVenuebyId(int id)
+        {
+            Venue venue = _dbContext.Venues.Where(e => e.venue_id == id).FirstOrDefault();
+            return venue;
+        }
+        public List<Venue> getAllVenues()
+        {
+            return _dbContext.Venues.ToList();
+        }
+        public bool addVenue(Venue venue)
+        {
+            _dbContext.Venues.Add(venue);
+            _dbContext.SaveChanges();
+            return true;
+        }
+
+
+    }
+}
