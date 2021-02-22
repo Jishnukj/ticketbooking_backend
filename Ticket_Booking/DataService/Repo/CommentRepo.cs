3441281aeaf0675d8b79475533266d8f2f5f9252
Parvathy Vinu <parvathy.vinu@experionglobal.local>
2021-02-22 16:17:24 +0530

    CommentRepo

---
+++ Ticket_Booking/DataService/Repo/CommentRepo.cs
@@ -0,0 +1,46 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataService.Repo
+{
+    public class CommentRepo : ICommentRepo
+    {
+        private readonly ApplicationdbContext _dbContext;
+        public CommentRepo(ApplicationdbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool add(Comment commentEntity)
+        {
+            _dbContext.Comments.Add(commentEntity);
+            _dbContext.SaveChanges();
+            return true;
+        }
+
+        public Comment getComment(int id)
+        {
+            Comment comment = _dbContext.Comments.Where(e => e.comment_id == id).FirstOrDefault();
+            return comment;
+        }
+
+        public Comment getCommentByUserId(int id)
+        {
+            Comment comment = _dbContext.Comments.Where(e => e.user_id == id).FirstOrDefault();
+            return comment;
+        }
+        public Comment getCommentByEventId(int id)
+        {
+            Comment comment = _dbContext.Comments.Where(e => e.event_id == id).FirstOrDefault();
+            return comment;
+        }
+
+        public List<Comment> getAllComments()
+        {
+            return _dbContext.Comments.ToList();
+        }
+    }
+}
---
+++ Ticket_Booking/DataService/Repo/ICommentRepo.cs
@@ -0,0 +1,16 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Repo
+{
+    public interface ICommentRepo
+    {
+        List<Comment> getAllComments();
+        Comment getComment(int id);
+        Comment getCommentByUserId(int id);
+        Comment getCommentByEventId(int id);
+        bool add(Comment commentEntity);
+    }
+}
