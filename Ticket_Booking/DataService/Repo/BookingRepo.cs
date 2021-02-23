fa74c59e6e7364d46ff62bb362d18180e807a2e5
Ancilla Philip <ancilla.philip@experionglobal.local>
2021-02-23 08:08:33 +0530

    Booking Repo

---
+++ Ticket_Booking/DataService/Repo/BookingRepo.cs
@@ -0,0 +1,32 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataService.Repo
+{
+    public class BookingRepo : IBookingRepo
+    {
+        private readonly ApplicationdbContext _dbContext;
+        public VenueRepo(ApplicationdbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Booking getBookingbyId(int id)
+        {
+            Booking booking = _dbContext.Bookings.Where(e => e.booking_id == id).FirstOrDefault();
+            return booking;
+        }
+        public bool addBooking(Booking booking)
+        {
+            _dbContext.Booking.Add(booking);
+            _dbContext.SaveChanges();
+            return true;
+        }
+        
+    }
+
+}
+
---
+++ Ticket_Booking/DataService/Repo/IBookingRepo.cs
@@ -0,0 +1,16 @@
+﻿using DataService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataService.Repo
+{
+	public interface IBookingRepo
+	{
+		Booking getBookingbyId(int id);
+		bool addBooking(Booking booking);
+	}
+
+}
+
